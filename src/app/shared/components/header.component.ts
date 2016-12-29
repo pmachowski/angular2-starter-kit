4a6498312e66f58022550565d7f0253fdc82b230
Piotr Machowski <piotr@machowski.co>
2016-12-29 13:18:21 +0100

    chore(header): add contact info

--- src/app/shared/components/header.component.ts
+++ src/app/shared/components/header.component.ts
@@ -10,8 +10,8 @@ import { Component, ChangeDetectionStrategy, ViewEncapsulation } from '@angular/
         <a class="pure-menu-heading" href="">Angular 2 Starter Kit</a>
 
         <ul class="pure-menu-list">
-          <li class="pure-menu-item"><a href="#" class="pure-menu-link">Contact Author</a></li>
-          <li class="pure-menu-item"><a href="#" class="pure-menu-link">Star on GitHub</a></li>
+          <li class="pure-menu-item"><a href="mailto:peter@machowski.co" class="pure-menu-link">Contact Author</a></li>
+          <li class="pure-menu-item"><a href="https://github.com/pmachowski/angular2-starter-kit" class="pure-menu-link">Star on GitHub</a></li>
         </ul>
       </div>
     </div>
